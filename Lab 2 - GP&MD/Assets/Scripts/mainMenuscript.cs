a574b44f377b44e1d78c7d1a3df03817604b8fd5
ShaneConroy <shaneconroy2002@gmail.com>
2023-10-04 16:27:30 +0100

    Began Lab 2 - animation

--- Lab 2 - GP&MD/Assets/Scripts/mainMenuscript.cs
+++ Lab 2 - GP&MD/Assets/Scripts/mainMenuscript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class mainMenuscript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static mainMenuscript instance;
+    private void Awake()
     {
-        
-    }
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+
+        }
     }
 }
+
